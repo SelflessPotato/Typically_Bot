5dd733b0f1da0764ba73c1ee1e6f9464ea645ab2
Dave Caruso <dave@davecode.net>
2021-12-30 15:31:54 -0500

    changes requested

--- src/modules/sleep.ts
+++ src/modules/sleep.ts
@@ -1,4 +1,4 @@
-import { ChatCommand, OnEvent } from 'purplet';
+import { OnEvent } from 'purplet';
 
 const messages = [
   'Shshshshsh friendo!! You have mentioned the myth!!!!',
@@ -7,19 +7,12 @@ const messages = [
   'I think you meant "long nap"...',
 ];
 
-export default ChatCommand({
-  name: 'sleep',
-  description: "huh, what's that?",
-  async handle() {
-    const msg = messages[Math.floor(Math.random() * messages.length)];
-    this.reply(msg);
-  },
-});
-
-export const onmessage = OnEvent('messageCreate', (msg) => {
+export default OnEvent('messageCreate', (msg) => {
   if (msg.author.bot) return;
 
-  if (msg.content.includes('sleep')) {
-    msg.channel.send('aaa! sleep.');
+  if (msg.content.includes('.sleep')) {
+    setTimeout(() => {
+      msg.channel.send(messages[Math.floor(Math.random() * messages.length)]);
+    }, 1000);
   }
 });
