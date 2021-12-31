a606688ed0528496aeccbb1a427de5f3e3803fb1
Ellis Shore <eshore@sassynetwork.com>
2021-12-31 17:25:30 -0500

    Adds admin role requirement for /addsaying

--- src/modules/addsaying.ts
+++ src/modules/addsaying.ts
@@ -6,14 +6,18 @@ export default ChatCommand({
   description: 'Create a new saying about myth mentions',
   options: new OptionBuilder().string('message', 'The saying to add', true),
   handle(options) {
-    Saying.create({ message: options.message })
-      .then(() => {
-        console.log(`Added new saying: "${options.message}"`);
-        this.reply(`Successfully added saying: "${options.message}"`);
-      })
-      .catch(() => {
-        console.error(`Problem adding saying: "$${options.message}"`);
-        this.reply(`Problem adding saying: "$${options.message}"`);
-      });
+    if (!this.member.roles.cache.has(process.env.ADMIN_PING_ROLE)) {
+      this.reply({ephemeral: true, content: 'Sorry, you don\'t have permission to add a saying!'});
+    } else {
+      Saying.create({ message: options.message })
+          .then(() => {
+            console.log(`Added new saying: "${options.message}"`);
+            this.reply(`Successfully added saying: "${options.message}"`);
+          })
+          .catch(() => {
+            console.error(`Problem adding saying: "$${options.message}"`);
+            this.reply(`Problem adding saying: "$${options.message}"`);
+          });
+    }
   },
 });
